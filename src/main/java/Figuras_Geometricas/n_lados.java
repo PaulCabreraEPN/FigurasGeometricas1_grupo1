083862def68111e0199ef5a11ce45cfb953d239d
ITHAN <ithan.camacho@epn.edu.ec>
2024-05-31 17:50:45 -0500

    Clase n_lados

---
+++ src/main/java/Figuras_Geometricas/n_lados.java
@@ -0,0 +1,36 @@
+package Figuras_Geometricas;
+
+public class n_lados extends Regulares{
+
+    Double apotema;
+
+    public n_lados() {
+
+    }
+
+    public n_lados(int numero_lados, String nombre, Double area, Double perimetro, Double tamaño_lado, Double apotema) {
+        super(numero_lados, nombre, area, perimetro, tamaño_lado);
+        this.apotema = apotema;
+    }
+
+    public Double getApotema() {
+        return apotema;
+    }
+
+    public void setApotema(Double apotema) {
+        this.apotema = apotema;
+    }
+
+    //Metodos
+
+
+    @Override
+    public Double calcular_perimetro(){
+        return tamaño_lado*numero_lados;
+    }
+
+    @Override
+    public Double calcular_area() {
+        return (perimetro * apotema)/2;
+    }
+}
