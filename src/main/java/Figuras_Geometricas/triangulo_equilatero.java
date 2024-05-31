31bb3adbed896f40cf2fbcace807cde4ab381bae
ITHAN <ithan.camacho@epn.edu.ec>
2024-05-31 17:04:33 -0500

    Clase triangulos y subclase triangulo_equilatero

--- src/main/java/Figuras_Geometricas/triangulo_equilatero.java
+++ src/main/java/Figuras_Geometricas/triangulo_equilatero.java
@@ -2,7 +2,6 @@ package Figuras_Geometricas;
 
 public class triangulo_equilatero extends triangulos{
 
-    public triangulo_equilatero(){}
 
     public triangulo_equilatero(int numero_lados, String nombre, Double area, Double perimetro, Double tamaño_lado, Double base, Double altura) {
         super(numero_lados, nombre, area, perimetro, tamaño_lado, base, altura);
