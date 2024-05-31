dd9b977659c3dcc7b8c735f6492d78526cc7d51f
Ithan <ithan.camacho@epn.edu.ec>
2024-05-31 12:40:17 -0500

    Clase figuras 2D

--- src/main/java/Figuras_Geometricas/Regulares.java
+++ src/main/java/Figuras_Geometricas/Regulares.java
@@ -5,4 +5,7 @@ public class Regulares extends figuras_2d{
     public Regulares(int numero_lados, String nombre, Double area, Double perimetro, Double tamaño_lado) {
         super(numero_lados, nombre, area, perimetro, tamaño_lado);
     }
+
+    public Regulares() {
+    }
 }
---
+++ src/main/java/Figuras_Geometricas/triangulos.java
@@ -0,0 +1,15 @@
+package Figuras_Geometricas;
+
+public class triangulos extends Regulares{
+
+    public triangulos() {
+    }
+
+    public triangulos(int numero_lados, String nombre, Double area, Double perimetro, Double tamaño_lado) {
+        super(numero_lados, nombre, area, perimetro, tamaño_lado);
+    }
+
+
+
+
+}
