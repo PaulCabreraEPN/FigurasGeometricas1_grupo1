1f8984eb08904a74e2377fee380f0b2a66be72d5
Anthony <anthony.astudillo@epn.edu.ec>
2024-05-31 17:21:36 -0500

    RECTANGULO ANTHONY

--- src/main/java/Figuras_Geometricas/Rectangulo.java
+++ src/main/java/Figuras_Geometricas/Rectangulo.java
@@ -1,4 +1,20 @@
 package Figuras_Geometricas;
 
-public class Rectangulo {
+public class Rectangulo extends cuadrilateros{
+
+    Double base;
+    Double altura;
+
+    public Rectangulo() {
+    }
+
+    public Rectangulo(int numero_lados, String nombre, Double area, Double perimetro, Double tamaño_lado) {
+        super(numero_lados, nombre, area, perimetro, tamaño_lado);
+    }
+
+    @Override
+    public Double calcular_area() {
+        area = base * altura;
+        return area;
+    }
 }
