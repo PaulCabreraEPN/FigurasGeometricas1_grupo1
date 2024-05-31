6a2d4203a6fd91e54bba02f937da06e5d75c8536
ITHAN <ithan.camacho@epn.edu.ec>
2024-05-31 17:18:57 -0500

    Correcci+on de figuras Triangulos

--- src/main/java/Figuras_Geometricas/triangulo_equilatero.java
+++
@@ -1,9 +0,0 @@
-package Figuras_Geometricas;
-
-public class triangulo_equilatero extends triangulos{
-
-
-    public triangulo_equilatero(int numero_lados, String nombre, Double area, Double perimetro, Double tamaño_lado, Double base, Double altura) {
-        super(numero_lados, nombre, area, perimetro, tamaño_lado, base, altura);
-    }
-}
